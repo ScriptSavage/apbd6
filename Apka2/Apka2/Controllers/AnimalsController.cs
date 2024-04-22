24a67e8aee2e0ccf19672e666cf422efcb87b1ee
MAX <maksymiliangaleziowski@gmail.com>
2024-04-22 17:55:16 +0200

    dodano zmiany

--- Apka2/Apka2/Controllers/AnimalsController.cs
+++ Apka2/Apka2/Controllers/AnimalsController.cs
@@ -6,76 +6,97 @@ using Microsoft.Data.SqlClient;
 namespace Apka2.Controllers;
 
 [ApiController]
-//[Route("api/animals")]
 [Route("api/[controller]")]
 public class AnimalsController : ControllerBase
 {
-
-    [HttpGet]
-    public IActionResult GetAnimals()
-    {
-        // polaczenie do bazy
-        using SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("Default"));
-        connection.Open();
-        
-        
-        
-        //Definicja Command
-
-        SqlCommand command = new SqlCommand();
-        command.Connection = connection;
-        command.CommandText = "SELECT  * FROM Animal";
-        
-        // Wykonanie zapytania
-
-        var reader = command.ExecuteReader();
-
-        List<Animal> animals = new List<Animal>();
-
-        int idAnimalOrdinal = reader.GetOrdinal("IdAnimal");
-        int nameOrdinal = reader.GetOrdinal("Name");
-        
-        // odczyt wartosci
-        while (reader.Read())
-        {
-            animals.Add(new Animal()
-            {
-                IdAnimal = reader.GetInt32(idAnimalOrdinal),
-                Name = reader.GetString(nameOrdinal)
-            });    
-        }
-        
-        return Ok(animals);
-    }
-
     private readonly IConfiguration _configuration;
-
     public AnimalsController(IConfiguration configuration)
     {
         _configuration = configuration;
-        
     }
 
-
+    [HttpGet]
+    public IActionResult GetAnimals([FromQuery] string orderBy = "Name")
+    {
+        orderBy = new List<string> { "Name", "Description", "Category", "Area" }.Contains(orderBy) ? orderBy : "Name";
+        List<Animal> animals = new List<Animal>();
+        using (var connection = new SqlConnection(_configuration.GetConnectionString("Default")))
+        {
+            connection.Open();
+            var query = $"SELECT * FROM Animal ORDER BY {orderBy}";
+            using (var command = new SqlCommand(query, connection))
+            {
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        animals.Add(new Animal()
+                        {
+                            IdAnimal = (int)reader["IdAnimal"],
+                            Name = reader["Name"].ToString(),
+                            Description = reader["Description"].ToString(),
+                            Category = reader["Category"].ToString(),
+                            Area = reader["Area"].ToString()
+                        });
+                    }
+                }
+            }
+        }
+        return Ok(animals);
+    }
 
     [HttpPost]
-    public IActionResult AddAnimal(AddAnimal addAnimal)
+    public IActionResult AddAnimal([FromBody] AddAnimal addAnimal)
     {
-        // polaczenie do bazy
-        using SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("Default"));
-        connection.Open();
-        
-        
-        
-        //Definicja Command
-
-        SqlCommand command = new SqlCommand();
-        command.Connection = connection;
-        command.CommandText = $"INSERT INTO Animal VALUES('{addAnimal}','','','')";
-        //command.Parameters.AddWithValue(@animalName, addAnimal.Name);
-
-        return Created();
+        using (var connection = new SqlConnection(_configuration.GetConnectionString("Default")))
+        {
+            connection.Open();
+            var query = "INSERT INTO Animal (Name, Description, Category, Area) VALUES (@Name, @Description, @Category, @Area)";
+            using (var command = new SqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@Name", addAnimal.Name);
+                command.Parameters.AddWithValue("@Description", addAnimal.Description);
+                command.Parameters.AddWithValue("@Category", addAnimal.Category);
+                command.Parameters.AddWithValue("@Area", addAnimal.Area);
+                command.ExecuteNonQuery();
+            }
+        }
+        return CreatedAtAction(nameof(GetAnimals), new { addAnimal.Name }, addAnimal);
     }
 
+    [HttpPut("{idAnimal}")]
+    public IActionResult UpdateAnimal(int idAnimal, [FromBody] AddAnimal updateAnimal)
+    {
+        using (var connection = new SqlConnection(_configuration.GetConnectionString("Default")))
+        {
+            connection.Open();
+            var query = "UPDATE Animal SET Name = @Name, Description = @Description, Category = @Category, Area = @Area WHERE IdAnimal = @IdAnimal";
+            using (var command = new SqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@Name", updateAnimal.Name);
+                command.Parameters.AddWithValue("@Description", updateAnimal.Description);
+                command.Parameters.AddWithValue("@Category", updateAnimal.Category);
+                command.Parameters.AddWithValue("@Area", updateAnimal.Area);
+                command.Parameters.AddWithValue("@IdAnimal", idAnimal);
+                command.ExecuteNonQuery();
+            }
+        }
+        return NoContent();
+    }
 
+    [HttpDelete("{idAnimal}")]
+    public IActionResult DeleteAnimal(int idAnimal)
+    {
+        using (var connection = new SqlConnection(_configuration.GetConnectionString("Default")))
+        {
+            connection.Open();
+            var query = "DELETE FROM Animal WHERE IdAnimal = @IdAnimal";
+            using (var command = new SqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@IdAnimal", idAnimal);
+                command.ExecuteNonQuery();
+            }
+        }
+        return NoContent();
+    }
 }
--- Apka2/Apka2/DTOs/AddAnimal.cs
+++ Apka2/Apka2/DTOs/AddAnimal.cs
@@ -9,6 +9,19 @@ public class AddAnimal
     [Required]
     [MaxLength(200)]
     public string Name { get; set; }
+    
+    [Required]
+    [MaxLength(200)]
     public string Description { get; set; }
     
+    [Required]
+    [MaxLength(200)]
+    public string Category { get; set; }
+    
+    [Required]
+    [MaxLength(200)]
+    public string Area { get; set; }
+    
 }
+
+
